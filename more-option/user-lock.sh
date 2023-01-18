a5b172f5302476e5e96cf4273e3e8b6294c5f485
Internetvps <89685787+Internetvps@users.noreply.github.com>
2023-01-18 12:23:05 +0800

    Add files via upload

--- more-option/user-lock.sh
+++ more-option/user-lock.sh
@@ -1,6 +1,6 @@
 # Created by DRGVPN
 #wget https://github.com/${GitUser}/
-GitUser="matkilaupahlawan"
+GitUser="Internetvps"
 #IZIN SCRIPT
 MYIP=$(curl -sS ipv4.icanhazip.com)
 echo -e "\e[32mloading...\e[0m"
