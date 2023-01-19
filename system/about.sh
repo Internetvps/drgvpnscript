9a419dbba7a4c0a3c1db8cb5d44431d27c7a415d
Internetvps <89685787+Internetvps@users.noreply.github.com>
2023-01-19 10:39:41 +0800

    Add files via upload

--- system/about.sh
+++ system/about.sh
@@ -37,19 +37,19 @@ gl='\e[32;1m'
 clear
 echo ""
 figlet " SCRIPT" | lolcat
-figlet " BY DRGVPN" | lolcat
+figlet " BY DRG" | lolcat
 echo -e "\e[32;1m.-----------------------------------------------.\e[0m"
 echo -e "\e[32;1m|               \e[36;1mINFO SCRIPT VPS\e[0m                 \e[32;1m|\e[0m"
 echo -e "\e[32;1m'-----------------------------------------------'\e[0m"
-echo -e "           \e[031;1m> Premium Script By drgvpn <\e[0m" | lolcat
+echo -e "           \e[031;1m> Premium Script By DRGVPN <\e[0m" | lolcat
 echo -e " \e[0;32m_______________________________________________\e[0m"
 echo -e "\e[0;32m|    \e[1;35mFor OS Debian 10 & Ubuntu 20.04 64 bit     \e[0;32m|\e[0m"
-echo -e "\e[0;32m|  \e[1;35mFor VPS with KVM and VMWare drgvpn         \e[0;32m|\e[0m"
+echo -e "\e[0;32m|  \e[1;35mFor VPS with KVM and VMWare DRGVPN         \e[0;32m|\e[0m"
 echo -e "\e[0;32m|               \e[1;35mI HOPE YOU HAPPY                \e[0;32m|\e[0m"
 echo -e "\e[0;32m|_______________________________________________\e[0;32m|\e[0m"
 echo -e "\e[0;32m|                   \e[1;36mTHANKS TO                   \e[0;32m|\e[0m"
 echo -e "\e[0;32m|_______________________________________________\e[0;32m|\e[0m"
-echo -e "\e[0;32m|                   \e[1;35mDRGVPNSERVER                 \e[0;32m|\e[0m"
+echo -e "\e[0;32m|                   \e[1;35mDRGSERVER                 \e[0;32m|\e[0m"
 echo -e "\e[0;32m|                   \e[1;35mALLAH SWT                   \e[0;32m|\e[0m"
 echo -e "\e[0;32m|                   \e[1;35mMy Family                   \e[0;32m|\e[0m"
 echo -e "\e[0;32m|                    \e[1;35mGoogle                     \e[0;32m|\e[0m"
