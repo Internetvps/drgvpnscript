45e394af37b34391d0e98e1a911cfea95ab75ac8
Internetvps <89685787+Internetvps@users.noreply.github.com>
2023-04-15 21:50:46 +0800

    Update xraay.sh

--- menu/xraay.sh
+++ menu/xraay.sh
@@ -1202,6 +1202,16 @@ xraay
 # USER LOGIN VLESS WS
 function menu12 () {
 clear
+NC='\e[0m'
+DEFBOLD='\e[39;1m'
+RB='\e[31;1m'
+GB='\e[32;1m'
+YB='\e[33;1m'
+BB='\e[34;1m'
+MB='\e[35;1m'
+CB='\e[35;1m'
+WB='\e[37;1m'
+clear
 echo -n >/tmp/other.txt
 data=($(cat /usr/local/etc/xray/config.json | grep '^#=' | cut -d ' ' -f 2 | sort | uniq))
 echo -e "${BB}————————————————————————————————————————————————————${NC}"
