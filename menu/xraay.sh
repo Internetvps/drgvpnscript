0d50b809e1a678ecf9502e350bd644887c6ef27b
Internetvps <89685787+Internetvps@users.noreply.github.com>
2023-05-21 13:10:10 +0800

    Add files via upload

--- menu/xraay.sh
+++ menu/xraay.sh
@@ -811,16 +811,16 @@ clear
 echo -n >/tmp/other.txt
 data=($(cat /usr/local/etc/xray/config.json | grep '^#=' | cut -d ' ' -f 2 | sort | uniq))
 echo -e "${BB}————————————————————————————————————————————————————${NC}"
-echo -e "              ${WB}Vless User Login Account${NC}              "
+echo -e "              ${WB}Vmess User Login Account${NC}              "
 echo -e "${BB}————————————————————————————————————————————————————${NC}"
 for akun in "${data[@]}"; do
 if [[ -z "$akun" ]]; then
 akun="Tidak Ada"
 fi
 echo -n >/tmp/ipvmess.txt
-data2=($(cat /var/log/xray/access.log | tail -n 500 | cut -d " " -f 3 | sed 's/tcp://g' | cut -d ":" -f 1 | sort | uniq))
+data2=($(cat /var/log/xray/access1.log | tail -n 500 | cut -d " " -f 3 | sed 's/tcp://g' | cut -d ":" -f 1 | sort | uniq))
 for ip in "${data2[@]}"; do
-jum=$(cat /var/log/xray/access.log | grep -w "$akun" | tail -n 500 | cut -d " " -f 3 | sed 's/tcp://g' | cut -d ":" -f 1 | grep -w "$ip" | sort | uniq)
+jum=$(cat /var/log/xray/access1.log | grep -w "$akun" | tail -n 500 | cut -d " " -f 3 | sed 's/tcp://g' | cut -d ":" -f 1 | grep -w "$ip" | sort | uniq)
 if [[ "$jum" = "$ip" ]]; then
 echo "$jum" >>/tmp/ipvmess.txt
 else
@@ -1211,28 +1211,28 @@ for akun in "${data[@]}"; do
 if [[ -z "$akun" ]]; then
 akun="Tidak Ada"
 fi
-echo -n >/tmp/ipvmess.txt
-data2=($(cat /var/log/xray/access.log | tail -n 500 | cut -d " " -f 3 | sed 's/tcp://g' | cut -d ":" -f 1 | sort | uniq))
+echo -n >/tmp/ipvless.txt
+data2=($(cat /var/log/xray/access2.log | tail -n 500 | cut -d " " -f 3 | sed 's/tcp://g' | cut -d ":" -f 1 | sort | uniq))
 for ip in "${data2[@]}"; do
-jum=$(cat /var/log/xray/access.log | grep -w "$akun" | tail -n 500 | cut -d " " -f 3 | sed 's/tcp://g' | cut -d ":" -f 1 | grep -w "$ip" | sort | uniq)
+jum=$(cat /var/log/xray/access2.log | grep -w "$akun" | tail -n 500 | cut -d " " -f 3 | sed 's/tcp://g' | cut -d ":" -f 1 | grep -w "$ip" | sort | uniq)
 if [[ "$jum" = "$ip" ]]; then
-echo "$jum" >>/tmp/ipvmess.txt
+echo "$jum" >>/tmp/ipvless.txt
 else
 echo "$ip" >>/tmp/other.txt
 fi
-jum2=$(cat /tmp/ipvmess.txt)
+jum2=$(cat /tmp/ipvless.txt)
 sed -i "/$jum2/d" /tmp/other.txt >/dev/null 2>&1
 done
-jum=$(cat /tmp/ipvmess.txt)
+jum=$(cat /tmp/ipvless.txt)
 if [[ -z "$jum" ]]; then
 echo >/dev/null
 else
-jum2=$(cat /tmp/ipvmess.txt | nl)
+jum2=$(cat /tmp/ipvless.txt | nl)
 echo "user : $akun"
 echo "$jum2"
 echo -e "${BB}————————————————————————————————————————————————————${NC}"
 fi
-rm -rf /tmp/ipvmess.txt
+rm -rf /tmp/ipvless.txt
 done
 rm -rf /tmp/other.txt
 echo ""
